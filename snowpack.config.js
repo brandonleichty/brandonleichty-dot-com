3769cc8afbb21af94c3a7f36ba194b585e61d451
Brandon Leichty <brandonl@me.com>
2021-03-30 15:53:34 -0500

    initial project setup (with Snowpack and Tailwind)

---
+++ snowpack.config.js
@@ -0,0 +1,10 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    src: "/",
+  },
+  plugins: ["@snowpack/plugin-postcss"]
+};
