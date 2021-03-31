be77617942e861355b9c684e0403b558788d52f1
Brandon Leichty <brandonl@me.com>
2021-03-30 21:40:19 -0500

    add custom tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,30 @@
 module.exports = {
   purge: ["./src/**/*.html", "./src/**/*.js"],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "class", // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        extend: {
+          brand: {
+            favPink: "#f02e65",
+            favWhite: "#ffffff",
+            favLightGray: "#40404c",
+            favDarkerGray: "#8f8f8f",
+            appleGreen: "#5EBD3E",
+            appleYellow: "#FFB900",
+            appleOrange: "#F78200",
+            appleRed: "#E23838",
+            applePurple: "#973999",
+            appleBlue: "#009CDF",
+            navy: "#111827",
+          },
+        },
+      },
+    },
   },
+
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
